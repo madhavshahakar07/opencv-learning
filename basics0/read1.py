9b0aab6c9d6c6b8fb24b10f5b8c137872af4590f
Madhav shahakar <madhavshshakar18101@gmail.com>
2026-09-26 00:10:40 +0530

    learned rescaleing videos python is feeling heavy from here need to up my game

--- basics0/read1.py
+++ basics0/read1.py
@@ -1,9 +1,12 @@
 import cv2 as cv
+#code for reading images
 '''
 img = cv.imread('C:\\opencv-learning\\resources\\photos\\cat_large.jpg')
 
 cv.imshow('Cat', img)
+cv.waitKey(0) #waits for a key to be pressed
 '''
+# code for reading videos
 capture = cv.VideoCapture('C:\\opencv-learning\\resources\\videos\\dog.mp4')
 while True:
     isTrue, frame = capture.read()#reads the video frame by frame
